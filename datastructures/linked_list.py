6e3746be31b1d9ae399c40a4374303194e6766f8
abstrxtt <bmtaki335@gmail.com>
2022-12-13 19:51:49 +0900

    initial commit

---
+++ datastructures/linked_list.py
@@ -0,0 +1,9 @@
+class LinkedListNode:
+    def __init__(self, value=None):
+        self.value = value
+        self.nextNode = None
+
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
